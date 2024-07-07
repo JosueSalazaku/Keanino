a607b4ee6652643369d9a945ec6244665968b365
Josué Salazaku <josuesalazaku@gmail.com>
2024-07-07 11:50:40 +0200

    Create getUser request

---
+++ src/app/lib/supabaseRequests.ts
@@ -0,0 +1,18 @@
+import { supabaseClient } from "./supabaseClient";
+ import { User } from '../../types';
+
+ export const supabaseRequests = async (supabaseToken: string) => {
+    const supabase = await supabaseClient(supabaseToken);
+  
+     const getUsers = async (): Promise<User[]> => {
+        const { data, error } = await supabase.from('users').select('*');  
+      if (error) throw error;
+  
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return (data ?? []) as User[];
+    };
+  
+    return {
+      getUsers,
+    };
+  };
