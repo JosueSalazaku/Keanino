4e4f811cf289f7555e86b215105c54f441c94bd8
Josué Salazaku <josuesalazaku@gmail.com>
2024-08-07 13:54:02 +0200

    add form styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -37,11 +37,11 @@ import { login, signup } from '../login/actions'
 
 export default function LoginPage() {
   return (
-    <form>
+    <form className='flex flex-col font-didot text-2xl items-center mt-5'>
       <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
+      <input id="email" name="email" type="email" required className='border-2 border-primary rounded-l' />
       <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
+      <input id="password" name="password" type="password" required className='border-2 border-primary rounded-l' />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
