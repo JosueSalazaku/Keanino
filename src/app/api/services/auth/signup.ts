3b89236d9a96c7e4a93f888dadba1f89d4d25c44
Josué Salazaku <josuesalazaku@gmail.com>
2024-08-09 23:20:59 +0200

    Add singup route handler

---
+++ src/app/api/services/auth/signup.ts
@@ -0,0 +1,17 @@
+import { signupAction } from '../../services/auth/actions'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { email, password } = req.body as { email: string, password: string };
+
+    const formData = new FormData()
+    formData.append('email', email)
+    formData.append('password', password)
+
+    await signupAction(formData)
+    res.redirect('/')
+  } else {
+    res.status(405).end() // Method Not Allowed
+  }
+}
