2f7a41da8029228309bee2d0d2e37eaa0641fb66
Josué Salazaku <josuesalazaku@gmail.com>
2024-08-29 20:52:34 +0200

    refactor: Update set up clerck auth

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { IoMdClose } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
 
 export function TopNav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,18 +17,20 @@ export function TopNav() {
         Keanino
       </Link>
       <button onClick={toggle} className="md:hidden">
-      {isOpen ? <IoMdClose className="size-8 text-white" /> : <GiHamburgerMenu className="size-8 text-white" />}
+        {isOpen ? <IoMdClose className="size-8 text-white" /> : <GiHamburgerMenu className="size-8 text-white" />}
       </button>
       <div className="hidden md:flex flex-row items-center text-white space-x-6">
         <Link href="/People">People</Link>
         <Link href="/Places">Places</Link>
         <Link href="/Pages">Pages</Link>
-        <Link href="/signup">
-          <button>Sign up</button>
-        </Link>
-        <Link href="/login">
-          <button>Log In</button>
-        </Link>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button>Login</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
 
       {/* Sliding Menu for Small Screens */}
@@ -36,12 +39,14 @@ export function TopNav() {
           <Link href="/People">People</Link>
           <Link href="/Places">Places</Link>
           <Link href="/Pages">Pages</Link>
-          <Link href="/signup">
-            <button>Sign up</button>
-          </Link>
-          <Link href="/login">
-            <button>Log In</button>
-          </Link>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button>Login</button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       )}
     </nav>
