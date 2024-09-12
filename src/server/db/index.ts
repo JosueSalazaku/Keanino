770dc09bc405c48d5ca994d539f5ec5e4c06dd54
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-12 22:21:24 +0200

    Refactor database code to remove unused password field in the users table

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,9 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { users } from './schema';
 import * as schema from './schema';
 
 const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
 export const db = drizzle(sql, { schema });
 
-const result = await db.select().from(users);
