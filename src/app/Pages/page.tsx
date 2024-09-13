f1685d5cb7203f6437a3351f658764a8b8aa68f4
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-14 01:14:35 +0200

    Refactor page component to use conditional rendering for signed-in and signed-out users

--- src/app/Pages/page.tsx
+++ src/app/Pages/page.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { SignedIn, SignedOut } from '@clerk/nextjs';
+import Link from 'next/link';
 
 export default function Pages() {
   return (
-    <div>Pages</div>
-  )
+    <>
+      <SignedIn>
+        <h1>Welcome to Pages</h1>
+        {/* Add your content here */}
+      </SignedIn>
+      <SignedOut>
+        <p>Please <Link href="/sign-in">sign in</Link> to view this content.</p>
+      </SignedOut>
+    </>
+  );
 }
-
