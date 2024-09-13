9d8f79ec92d1bf15115b2063fcc244a5123992a5
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-14 00:57:12 +0200

    Refactor TopNav component to use lowercase URLs

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -19,9 +19,9 @@ export function TopNav() {
       </button>
       <div className="hidden md:flex flex-row items-center text-white space-x-6">
         <SignedIn>
-          <Link href="/People">People</Link>
-          <Link href="/Places">Places</Link>
-          <Link href="/Pages">Pages</Link>
+          <Link href="/people">People</Link>
+          <Link href="/places">Places</Link>
+          <Link href="/pages">Pages</Link>
           <UserButton />
           <button onClick={() => signOut()}>Sign Out</button>
         </SignedIn>
@@ -35,9 +35,9 @@ export function TopNav() {
       {isOpen && (
         <div className="absolute top-20 left-0 right-0 z-50 bg-orange-400 p-5 flex flex-col text-6xl space-y-10 text-main md:hidden">
           <SignedIn>
-            <Link href="/People">People</Link>
-            <Link href="/Places">Places</Link>
-            <Link href="/Pages">Pages</Link>
+            <Link href="/people">People</Link>
+            <Link href="/places">Places</Link>
+            <Link href="/pages">Pages</Link>
             <UserButton />
             <button onClick={() => signOut()}>Sign Out</button>
           </SignedIn>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
 import CreatePost from "~/components/createPost";
+import { SignedIn, SignedOut } from "@clerk/nextjs"; 
 
-
-export default async function HomePage() {
-
-
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start pt-12 bg-primary text-white">
       <section className="mt-10 gap-7">
-      The 3 P
+        The 3 P
       </section>
-      <section>
-        <CreatePost/>
-      </section>
-      
+
+      {/* Show CreatePost when signed in */}
+      <SignedIn>
+        <section>
+          <CreatePost />
+        </section>
+      </SignedIn>
+
+      {/* Show something else when signed out */}
+      <SignedOut>
+        <section>
+          <p>Please sign in to create a post.</p>
+        </section>
+      </SignedOut>
     </main>
   );
 }
-
