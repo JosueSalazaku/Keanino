0c103a581c2b907f51020d40dda26f96019c7fb7
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-25 16:08:24 +0200

    Add logic to close Menu after clicking a link #14

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -10,6 +10,7 @@ export function TopNav() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   const { signOut } = useAuth();
+  const closeMenu = () => setIsOpen(false)
 
   return (
     <nav className="flex h-20 w-full items-center justify-between bg-primary px-14">
@@ -36,17 +37,17 @@ export function TopNav() {
 
       {isOpen && (
         <div className="absolute top-20 left-0 right-0 z-50 bg-orange-400 p-5 flex flex-col text-6xl space-y-10 text-main md:hidden">
-          <SignedIn>
-            <Link href="/write">write</Link>
-            <Link href="/people">People</Link>
-            <Link href="/places">Places</Link>
-            <Link href="/pages">Pages</Link>
-            <button onClick={() => signOut()}>Sign Out</button>
-          </SignedIn>
-          <SignedOut>
-            <Link href="/sign-in"><button>Sign In</button></Link>
-            <Link href="/sign-up"><button>Sign Up</button></Link>
-          </SignedOut>
+            <SignedIn>
+            <Link href="/write" onClick={closeMenu}>write</Link>
+            <Link href="/people" onClick={closeMenu}>People</Link>
+            <Link href="/places" onClick={closeMenu}>Places</Link>
+            <Link href="/pages" onClick={closeMenu}>Pages</Link>
+            <button onClick={async () => { await signOut(); closeMenu(); }}>Sign Out</button>
+            </SignedIn>
+            <SignedOut>
+            <Link href="/sign-in"><button onClick={closeMenu}>Sign In</button></Link>
+            <Link href="/sign-up"><button onClick={closeMenu}>Sign Up</button></Link>
+            </SignedOut>
         </div>
       )}
     </nav>
