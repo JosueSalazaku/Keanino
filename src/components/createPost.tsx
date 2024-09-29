7f40c91f209df73f6d080335818a8535c7a2270d
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-29 14:27:48 +0200

    Refactor: fix tyoescript bugs in createPost component

--- src/components/createPost.tsx
+++ src/components/createPost.tsx
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
-/* eslint-disable @typescript-eslint/prefer-optional-chain */
+
 "use client";
 import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import { Button } from "./ui/button";
-// import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import axios from "axios";
 import { useUser } from "@clerk/nextjs";
@@ -15,8 +13,8 @@ export default function CreatePost() {
   const initialTitle = searchParams.get('title');
   const initialContent = searchParams.get('content');
 
-  const [submitTitle, setSubmitTitle] = useState<string>(initialTitle || "");
-  const [submitContent, setSubmitContent] = useState<string>(initialContent || "");  
+  const [submitTitle, setSubmitTitle] = useState<string>(initialTitle! || "");
+  const [submitContent, setSubmitContent] = useState<string>(initialContent! || "");  
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -25,7 +23,7 @@ export default function CreatePost() {
   async function handlePost(event: React.FormEvent) {
     event.preventDefault();
 
-    if (!user || !user.id) {
+    if (!user?.id) {
       setError("User not authenticated.");
       return;
     }
@@ -62,10 +60,10 @@ export default function CreatePost() {
   }
 
   return (
-    <div className="w-full flex justify-center mt-8">
+    <div className="w-screen flex justify-center mt-8 px-12">
       <form 
         onSubmit={handlePost} 
-        className="w-screen h-fit  flex flex-col space-y-6 p-4 bg-white text-black shadow-lg rounded-lg"
+        className="w-screen h-fit  flex flex-col space-y-6 p-4 bg-white text-black shadow-lg "
       >
         <Textarea
           value={submitTitle}
